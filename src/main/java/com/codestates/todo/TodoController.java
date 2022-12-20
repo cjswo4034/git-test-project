411fd40dedc7737dad4ae6a719c678a80bf5954a
cjswo4034 <cjswo4034@gmail.com>
2022-12-20 22:02:38 +0900

    a

--- src/main/java/com/codestates/todo/TodoController.java
+++ src/main/java/com/codestates/todo/TodoController.java
@@ -1,2 +1,15 @@
-package com.codestates.todo;public class TodoController {
+package com.codestates.todo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TodoController {
+    @GetMapping
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("To-do Application!");
+    }
 }
